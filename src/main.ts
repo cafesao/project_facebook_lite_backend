ca45b13f68d2a64f3e2f91bcc6d7c8449dc91790
cafesao <my@cafesao.net>
2024-01-31 18:52:28 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { configDotenv } from 'dotenv';
 import { Logger } from '@nestjs/common';
@@ -6,6 +7,16 @@ configDotenv();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Facebook Lite API')
+    .setDescription('The Facebook Lite API description')
+    .setVersion('0.1')
+    .addTag('facebook_lite')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
   Logger.log(`Listening on http://localhost:${process.env.PORT}`);
 }
